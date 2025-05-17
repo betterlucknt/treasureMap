70d111bf2f8cb96ef1a0515f202a34187e4e988d
Marc Serra <bonasort.bcn@gmail.com>
2025-05-17 12:59:18 +0200

    scenes ben posades

--- app/treasuremap/routes.py
+++ app/treasuremap/routes.py
@@ -13,14 +13,13 @@ COUNTDOWN_SUBSTRACT = 10
 START_STEP = 3
 
 # TODO:
-#        - Reordenar ajudes cornipulpo
+#        - play from admin
+#        - stop audio
 #        - imatges adients
 #        - Posar alguna gemma en la pagina
-#        - Posar les gemmes que s-activin a mida que les aconsegueixen
-#        - Posar opcio d'afegir o treure temps en la config de la prova
-#        - Afegir proba buida quan no hagin de fer servir el ordi
-#        - Add temps en step per mirar si poden demanar pista('popunicorn') o no
-# Audios: octocorn->baby; drxorç->prot.Tone34.chitter77
+#        - Posar les gemmes que s'activin a mida que les aconsegueixen
+#        - Check Needs redirect per actualitzar la pagina si hi ha canvi de current_scene
+# Audios: octocorn->baby; drxorç->prot.Tone34.chitter77; unicorns -> Necromancer
 
 
 scenes = {
@@ -28,71 +27,115 @@ scenes = {
         "image": "images/cute_baby_unicorn.jpeg",
         "text": '''Hola nens i nenes de la Terra, us contactem desde el món dels unicorns perquè necessitem la vostre ajuda!!
 Un cièntific boig vol crear un raig per destruïr el Sol i per aconseguir-ho necessita el poder de les gemmes màgiques del nostre món. No sabem com ha aconseguit arribar al món Unicorn i nosaltres hem hagut d'amagar les gemmes a la Terra per tal de protegir-les. El científic boig es diu Doctor Xoriçó i ha espatllat el mecanisme que ens ha permés obrir el portal per enviar-vos les gemmes i ara no tenim prou màgia per venir-les a buscar. Hem pogut enviar 4 gemmes, la Gemma del Nord, la del Sud, la de l'Est i la de l'Oest, però sense elles no podem reparar el portal, només hem aconseguit enviar-vos un petit teletransportador que comunica la Terra amb el món dels Unicorns. Necessitem que vosaltres les trobeu i les poseu al teletransportador per tal que ens arribin i junts poguem vèncer el Dr. Xoriço. Compte!! ara no sabem on pare aquest Dr. torrat i podria estar aprop vostre també buscant les gemmes, de ben segur que haurà posat trampes per si algú més les està buscant. Nosaltres sabem a on són les 4 gemmes però per si el Dr. Xoriço està espiant no us podem dir a on s'amaguen, però si us hem deixat un mapa i pistes perquè les pogueu trobar totes. Seguiu el mapa i si us perdeu no dubteu a preguntar a través de l'ordinador, la nostre mascota Popunicorn us ajudarà sempre que pugui. Sempre que poguem ens comunicarem a través de l'ordinador de la Base Secreta on us trobeu ara, molta sort!''',
+        "question": "", 
+        "answer": "", 
+    },
+    2: {
+        "image": "images/cute baby unicorn (1).jpeg",
+        "text": '''''',
         'octocorn': '''Hi ha alguna "illa" en aquesta casa?''',
-        'octocorn_image': "images/sticker_1.avif",
-        "question": "", 
-        "answer": "", 
-    },
-    2: {
-        "image": "images/Designer (27).webp",
-        "text": f'''Dr. Xoriço: Heu estat contagiats per un virus i us queden {COUNTDOWN_MINUTES} min abans no faci efecte i us quedeu adormits per mesos. Ja ja ja, les gemes seran meves.''',
-        'octocorn': '''"Nuvols" és la última paraula de la frase''',
-        'octocorn_image': "images/sticker_1.avif",
-        "question": "", 
-        "answer": "", 
-    },
-    2: {
-        "image": "images/sticker_1.avif",
-        "text": f'''Ostres no, el Dr. Xoriço ens ha enganyat, però no us preocupeu, els unicorns us prepararem l'antidot per parar el virus. 
-        Vosaltres seguiu buscant les gemes que no queda gaire temps. Seguiu el Mapa! ''',
-        'octocorn': '''"Nuvols" és la última paraula de la frase''',
-        'octocorn_image': "images/sticker_1.avif",
+        'octocorn_image': "images/sticker_28.avif",
         "question": "", 
         "answer": "", 
     },
     3: {
-        "image": "images/cute_baby_unicorn.jpeg",
-        "text": "Oh no! el DR. Xoriço està començant a tapar el sol utilitzant energia que ha robat del nostre món, afanyeu-vos, necessitem les gemmes o aquest malvat es sortirà amb la seva.",
-        'octocorn': '''Un dels Pokemons és Psyduck''',
-        'octocorn_image': "images/sticker_1.avif",
+        "image": "images/Designer (24).jpeg",
+        "text": f'''''',
+        'octocorn': '''Crec que "Cofre" i "Nuvols" estan a la sopa''',
+        'octocorn_image': "images/sticker_2.avif",
         "question": "", 
         "answer": "", 
     },
     4: {
-        "image": "images/cute_baby_unicorn.jpeg",
-        "text": "Moltes gràcies, amb la gemma de l'Est tornem a tenir una mica de màgia per poder-vos ajudar. Amb això podem retressar una estona l'efecte del virus, ara teniu 10 min més per seguir buscant les altres gemmes. Molta sort!",
+        "image": "images/Designer (26).jpeg",
+        "text": f'''''',
+        'octocorn': '''"Nuvols" és la última paraula de la frase''',
+        'octocorn_image': "images/sticker_20.avif",
         "question": "", 
         "answer": "", 
     },
     5: {
-        "image": "images/cute_baby_unicorn.jpeg",
-        "text": '''Com el nostre poder prové de la llum dels arcs de Sant Martí, haureu de recordar la seqüencia de llums en l'ordre correcte. Si ho aconseguiu obtindreu el codi.''',
+        "image": "images/Dr_xorico1.jpeg",
+        "text": f'''Heu estat contagiats per un virus i us queden {COUNTDOWN_MINUTES} min abans no faci efecte i us quedeu adormits per mesos. Ja ja ja, les gemes seran meves.''',
         "question": "", 
         "answer": "", 
     },
     6: {
-        "image": "images/cute_baby_unicorn.jpeg",
-        "text": '''Moltes gràcies, amb la gemma del Nord tornem a tenir una mica de màgia per poder-vos ajudar. Amb això podem retressar una estona l'efecte del virus, ara teniu 10 min més per seguir buscant les altres gemmes. Molta sort!''',
+        "image": "images/sticker_10.avif",
+        "text": f'''Ostres no, el Dr. Xoriço ens ha enganyat, però no us preocupeu, els unicorns us prepararem l'antidot per parar el virus. 
+        Vosaltres seguiu buscant les gemes que no queda gaire temps. Seguiu el Mapa! ''',
         "question": "", 
         "answer": "", 
     },
     7: {
-        "image": "images/cute_baby_unicorn.jpeg",
-        "text": '''Moltes gràcies, amb la gemma de l'Oest tornem a tenir una mica de màgia per poder-vos ajudar. Amb això podem retressar una estona l'efecte del virus, ara teniu 10 min més per seguir buscant la última gemma. Molta sort!''',
-        'octocorn': '''Hi ha alguna X en aquesta casa?''',
-        'octocorn_image': "images/sticker_1.avif",
+        "image": "images/cute baby unicorn (3).jpeg",
+        "text": "",
+        'octocorn': '''Un dels Pokemons és Psyduck''',
+        'octocorn_image': "images/sticker_21.avif",
         "question": "", 
         "answer": "", 
     },
     8: {
-        "image": "images/sticker_1.avif",
-        "text": '''El Dr. Xoriço va aconseguir estripar un troç del mapa, però no us preocupeu, amb tot el que heu aconseguit ja podeu trobar quin lloc indicava el mapa. No tardeu que encara heu de superar alguna prova per aconguir la Gemma del Sud!''',
-        'octocorn': '''Quines peces encara no heu utilitzat?''',
-        'octocorn_image': "images/sticker_1.avif",
+        "image": "images/Designer (13).jpeg",
+        "text": "Oh no! el DR. Xoriço està començant a tapar el sol utilitzant energia que ha robat del nostre món, afanyeu-vos, necessitem les gemmes o aquest malvat es sortirà amb la seva.",
         "question": "", 
         "answer": "", 
     },
     9: {
+        "image": "images/Designer (27).jpeg",
+        "text": "",
+        'octocorn': '''Quan a l'estiu fa molta calor, els gelats els guardem al........''',
+        'octocorn_image': "images/sticker_11.avif",
+        "question": "", 
+        "answer": "", 
+    },
+    10: {
+        "image": "images/Designer (30).jpeg",
+        "text": "Moltes gràcies, amb la gemma de l'Est tornem a tenir una mica de màgia per poder-vos ajudar. Amb això podem retressar una estona l'efecte del virus, ara teniu 10 min més per seguir buscant les altres gemmes. Molta sort!",
+        "question": "", 
+        "answer": "", 
+    },
+    11: {
+        "image": "images/Designer (29).jpeg",
+        "text": '''Com el nostre poder prové de la llum dels arcs de Sant Martí, haureu de recordar la seqüencia de llums en l'ordre correcte. Si ho aconseguiu obtindreu el codi.''',
+        "question": "", 
+        "answer": "", 
+    },
+    12: {
+        "image": "images/Designer (12).jpeg",
+        "text": '''Moltes gràcies, amb la gemma del Nord tornem a tenir una mica de màgia per poder-vos ajudar. Amb això podem retressar una estona l'efecte del virus, ara teniu 10 min més per seguir buscant les altres gemmes. Molta sort!''',
+        "question": "", 
+        "answer": "", 
+    },
+    13: {
+        "image": "images/cute baby unicorn (2).jpeg",
+        "text": '''''',
+        'octocorn': '''Hi ha alguna X en aquesta casa?''',
+        'octocorn_image': "images/sticker_10 (1).avif",
+        "question": "", 
+        "answer": "", 
+    },
+    14: {
+        "image": "images/Designer (25).jpeg",
+        "text": '''Moltes gràcies, amb la gemma de l'Oest tornem a tenir una mica de màgia per poder-vos ajudar. Amb això podem retressar una estona l'efecte del virus, ara teniu 10 min més per seguir buscant la última gemma. Molta sort!''',
+        "question": "", 
+        "answer": "", 
+    },
+    15: {
+        "image": "images/Designer (28).jpeg",
+        "text": '''El Dr. Xoriço va aconseguir estripar un troç del mapa, però no us preocupeu, amb tot el que heu aconseguit ja podeu trobar quin lloc indicava el mapa. No tardeu que encara heu de superar alguna prova per aconguir la Gemma del Sud!''',
+        "question": "", 
+        "answer": "", 
+    },
+    16: {
+        "image": "images/cute baby unicorn (1).jpeg",
+        "text": '''''',
+        'octocorn': '''Quines peces encara no heu utilitzat?''',
+        'octocorn_image': "images/sticker_5.avif",
+        "question": "", 
+        "answer": "", 
+    },
+    17: {
         "image": "images/cute_baby_unicorn.jpeg",
         "text": '''Moltes gràcies, amb la gemma del Sud ja hem recuperat tot el nostre poder. Ara el DR. Xoriço ja no podrà apagar el sol ni fer més malícies. Ràpid, pugeu tots a l'habitació de l'Alexandra, allà trobareu unes piruletes màgiques amb l'antídot pel virus.''',
         "question": "", 
@@ -188,6 +231,18 @@ def add_time():
     end_time = end_time + timedelta(minutes=COUNTDOWN_ADD)
     return str(True)
 
+@bp.route('/add_time_5')
+def add_time_5():
+    global end_time
+    end_time = end_time + timedelta(minutes=5)
+    return str(True)
+
+@bp.route('/add_time_1')
+def add_time_1():
+    global end_time
+    end_time = end_time + timedelta(minutes=1)
+    return str(True)
+
 @bp.route('/stop_time')
 def stop_time():
     global stopped_time
@@ -214,6 +269,18 @@ def resume_time():
 def substract_time():
     global end_time
     end_time = end_time - timedelta(minutes=COUNTDOWN_SUBSTRACT)
+    return str(True)
+
+@bp.route('/substract_time_5')
+def substract_time_5():
+    global end_time
+    end_time = end_time - timedelta(minutes=5)
+    return str(True)
+
+@bp.route('/substract_time_1')
+def substract_time_1():
+    global end_time
+    end_time = end_time - timedelta(minutes=1)
     return str(True)
 
 @bp.route('/advance_scene')
